875b617d3f363c5ac6cfd178d279347543982303
NDily1230 <ndily1230@gmail.com>
2016-10-29 09:37:12 -0400

    test specs

--- test/spec/test.js
+++ test/spec/test.js
@@ -5,24 +5,15 @@ var format_names = require('../testServer.js'),
 
 describe("format_names", function () {
 
-  it("should throw if it doesn't receive an array", function () {
-    (function () { format_names("HAHAHA!") }).should.throw(Error);
+  it("should throw if user name empty", function () {
+    (function () { format_names("") }).should.throw(Error);
   })
 
-  it("should return empty string when it gets an empty list", function () {
-    (format_names([])).should.equal('');
+  it("should throw error if email empty ", function () {
+    (function () { format_names("") }).should.throw(Error);
   });
 
-  it("should print a single name if it only receives one", function () {
-    (format_names(['Pushkin'])).should.equal('Pushkin');
+  it("should throw error if password empty", function () {
+    (function () { format_names("") }).should.throw(Error);
   });
-
-  it("should print names separated with an ampersand if it receives two", function () {
-    (format_names(['Pushkin', 'Gogol'])).should.equal('Pushkin & Gogol');
-  });
-
-  it("should separate initial names with commas and last with ampersand for long lists", function () {
-    (format_names(['Pushkin', 'Gogol', 'Dostoevsky'])).should.equal('Pushkin, Gogol & Dostoevsky');
-  });
-
 })
